c7d7c7849f24696ff4ba373f2918203a26b88e8f
Kseniya <baharevaxen@yandex.ru>
2025-08-15 19:23:22 +0300

    adding currency

---
+++ src/services.py
@@ -0,0 +1,35 @@
+import json
+import logging
+import re
+from typing import List, Dict
+
+
+# логирование
+logging.basicConfig(level=logging.INFO)
+
+
+def search_trans(transactions: List[Dict]) -> str:
+    """ Функция ищет переводы по физическим лицам """
+    logging.info("Начало поиска")
+    filtered_transactions = []
+    pattern = re.compile(r"^[А-ЯЁ][а-яё]+\s[А-ЯЁ]\.$")
+    """ Проверяем есть ли категории переводы"""
+    logging.info("Происходит поиск по фильтру: Переводы в Категории")
+    for trans in transactions:
+        if trans.get("Категории") == "Переводы" and pattern.search(trans.get("Описание", "")):
+            filtered_transactions.append(trans)
+
+    logging.info(f"Найдено {len(filtered_transactions)} переводов физическим лицам")
+    return json.dumps(filtered_transactions,ensure_ascii=False)
+
+if __name__ == "__main__":
+    transactions = [
+        {"Категории": "Переводы", "Описание": "Валерий А.", "Количество": 1000},
+        {"Категории": "Покупки", "Описание": "Роман В.", "Количество": 500},
+        {"Категории": "Переводы", "Описание": "Сергей З.", "Количество": 500},
+        {"Категории": "Покупки", "Описание": "Артем П.", "Количество": 1500}
+    ]
+
+    result = search_trans(transactions)
+    print(result)
+
